791f1b54f6a84f9a34aae696fc7d70e5d6bacafc
xygirl <1498546245@qq.com>
2017-06-27 22:52:32 +0800

    模拟购物车数据，生成简单的购物车页面

--- Vue-project/README.md
+++ Vue-project/README.md
@@ -1,6 +1,6 @@
 # vue-project
 
-> 易用，灵活，高效（虚拟dom）
+> 易用，灵活，高效（虚拟dom）操作模型data,而不直接操作DOM
 
 ## Build Setup
 
@@ -25,8 +25,10 @@ npm run build --report
 -  v-modal
 -  v-if,v-show,
 -  v-for循环渲染列表
--  v-on事件绑定
-- v-bind属性操作，class操作 对象，数组，：class 
+-  v-on:click事件绑定 <=>@click
+-  v-bind属性操作，url class操作 对象，数组，<=>：class 
+-  过滤器，computed属性
+
 
 ## vue实例 new Vue
 
@@ -56,3 +58,13 @@ new Vue({
     })
   ```
   ## 组件
+  ## vue-Resource与后台数据交互 axios
+  
+    就像jQuery里的$.ajax，用来和后台交互数据。
+  ## 过滤器/计算属性
+  
+  
+  ## 问题
+  
+  （1）使用vue监听不存在的变量，vm.set
+   (2) 使用$http.get('../')获得本地数据，数据应该放在static目录下
--- Vue-project/src/App.vue
+++ Vue-project/src/App.vue
@@ -1,54 +1,57 @@
 <template>
   <div id="app">
-    <h1 v-text="title"></h1>
-    <input v-model="newItem" v-on:keyup.enter="addNew">
-    <ul>
-      <li v-for="item in items" v-bind:class="{finished:item.isFinished}"
-       v-on:click="toggleFinish(item)">
-        {{item.label}}
-      </li>
-    </ul>
-    <p>child tells father : {{ childMsg }}</p>
-    <Components-hello msgfromfather="父组件给子组件传递的消息"
-    v-on:child-tell-father="listenToSon"></Components-hello>
+    <div class="cart">
+      <div class="cart-title">
+        <h1>购物车</h1>
+      </div>
+      <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+          <th><label for="check"><input type="checkbox" id="check"></label></th>
+          <th>商品信息</th>
+          <th>商品金额</th>
+          <th>商品数量</th>
+          <th>商品总额</th>
+          <th>编辑</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in productList">
+            <td><label for=""><input type="checkbox"></label></td>
+            <td><img v-bind:src="item.productImage" alt="商品描述" width="80px;height:80px;"></td>
+            <td><span>item.productPrice</span></td>
+            <td><span>-</span><input type="text" class="form-control" value="0" v-model="item.productQuantity" style="width:50px;"><span>+</span></td>
+            <td><span>{{item.productPrice*item.productQuantity}}</span></td>
+            <td><button class="btn">删除</button></td>
+          </tr>
+        </tbody>
+      </table>
+       <div class="cart-footer">
+          <div class="item-total">总价：<a href="#" class="btn">结账</a></div>
+       </div>
+      </div>
   </div>
+
 </template>
 
 <script>
-  import Store from './store'
-  import ComponentsHello from './components/Hello'
+
   export default {
     data: function () {
       return {
-        title: 'Vue test hello!',
-        items: Store.fetch(),
-        newItem: '',
-        childMsg: ''
+        productList: []
       }
     },
-    components: { ComponentsHello },
-    watch: {
-      items: {
-        handler: function (items) {
-          Store.save(items)
-        },
-        deep: true
-      }
+    mounted: function () {
+      this.$nextTick(function () {
+        this.cartView()
+      })
     },
     methods: {
-      toggleFinish: function (item) {
-        item.isFinished = !item.isFinished
-      },
-      addNew: function () {
-        this.items.push({
-          label: this.newItem,
-          isFinished: false
+      cartView: function () {
+        this.$http.get('../../static/data/data.json').then(res => {
+          this.productList = res.body.result.list
         })
-        this.newItem = ''
-      },
-      listenToSon: function (msg) {
-        console.log(msg)
-        this.childMsg = msg
       }
     }
 }
@@ -61,15 +64,74 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
 }
 #app ul {
   list-style: none;
 }
-#app ul li {
-  padding: 5px 10px;
+.cart {
+  width: 100%;
 }
-.finished {
-    color: red;
+  .cart-title {
+    width: 100%;
+    text-align: center;
+    color: #7fffd4;
   }
+
+ .table {
+   width: 100%;
+   border-collapse: collapse;
+   max-width: 100%;
+   margin-bottom: 20px;
+ }
+  .table > thead > tr > th,
+  .table > tbody > tr > td {
+    padding: 8px;
+    line-height: 1.4;
+    vertical-align: middle;
+    border-top: 1px solid #ddd;
+  }
+  .table-striped > tbody > tr:nth-of-type(odd) {
+    background-color: #f9f9f9;
+  }
+  .table-hover > tbody > tr:hover {
+    background-color: #f5f5f5;
+  }
+  .form-control {
+    display: inline-block;
+    padding: 6px 12px;
+    margin: 0 5px;
+    font-size: 14px;
+    color: #555;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+  }
+  .form-control:focus {
+    border-color: #66afe9;
+    outline: 0;
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(102, 175, 233, .6);
+
+  }
+  a {
+    text-decoration: none;
+  }
+  .btn {
+    display: inline-block;
+    padding: 6px 12px;
+    text-align: center;
+    cursor: pointer;
+    border-radius: 4px;
+    border: 1px solid transparent;
+    color: #000;
+    background-color: #7fffd4;
+
+  }
+  .btn:hover {
+    color: #000;
+    background-color: #8fffdd;
+    outline: none;
+  }
+
+
 </style>
